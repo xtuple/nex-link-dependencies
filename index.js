540367887df365c822ef35faeac563cbc9a0337c
Travis Webb <me@traviswebb.com>
2014-07-10 19:22:27 -0400

    added dependencies

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var path = require('path');
 var fs = require('fs');
 var proc = require('child_process');
 var color = require('colors');
+var log = require('npmlog');
 
 var handler = module.exports = new nex.Handler('linkDependencies');
 
@@ -16,12 +17,12 @@ var handler = module.exports = new nex.Handler('linkDependencies');
 handler.do = function (pkg) {
   _.each(pkg[this.field], function (dir, name) {
     process.chdir(dir);
-    this.log.info('npm install', name, 'in'.magenta, dir);
+    log.info('npm install', name, 'in'.magenta, dir);
     proc.spawnSync('npm',[ 'install' ], { cwd: dir });
     process.chdir(global.cwd);
 
     let linkName = path.resolve(process.cwd(), 'node_modules', name);
-    this.log.info('link', path.relative(process.cwd(), linkName), '->', dir);
+    log.info('link', path.relative(process.cwd(), linkName), '->', dir);
 
     rimraf.sync(linkName);
     fs.symlinkSync(path.resolve(dir), linkName);
